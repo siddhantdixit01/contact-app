0702b09bed77a75b25ac0f1950389c916f398cb6
Siddhant Dixit <114592704+siddhantdixit01@users.noreply.github.com>
2023-10-27 17:21:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoUri = 'mongodb+srv://siddhantdixit97:StG7MYhISzSAOdgy@cluster0.cqtxqdf.mongodb.net/contacts';
 
-app.use(cors());
+app.use(cors(
+  origin:["https://contact-app-lake-rho.vercel.app/"],
+  methods:["POST","GET"],
+  credentials: true
+));
 app.use(express.json());
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
